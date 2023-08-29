b4fe7330b2e674b0c2e895450226cb3d42a9d805
Yatharth Verma <yatharthverma@Yatharths-MacBook-Pro.local>
2023-08-29 23:12:53 +0530

    Add toggle bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="p-10 flex flex-col">
         <h2 className="text-xl font-normal">Framer Components</h2>
 
-        <main className="flex space-x-4">
+        <main className="flex space-x-4 py-6">
           <ToggleBar />
         </main>
       </div>
--- src/framer-components/ToggleBar.jsx
+++ src/framer-components/ToggleBar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 function ToggleBar() {
-  return <div>ToggleBar</div>;
+  const [isToggleOn, setIsToggleOn] = useState(false);
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+
+  return (
+    <div
+      className={
+        "bg-blue-500 w-20 h-10 rounded-full flex items-center p-1 cursor-pointer" +
+        (isToggleOn ? " justify-end" : " justify-start")
+      }
+      onClick={() => setIsToggleOn(!isToggleOn)}
+    >
+      <motion.div
+        className="rounded-full w-7 h-7 bg-white"
+        layout
+        transition={spring}
+      ></motion.div>
+    </div>
+  );
 }
 
 export default ToggleBar;
