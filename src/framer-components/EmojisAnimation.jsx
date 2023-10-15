81a33e9e6f8a3948da19e3659ce891bcc007e3cb
Yatharth Verma <yatharthverma@Yatharths-MacBook-Pro.local>
2023-10-15 20:57:02 +0530

    feat: Add Dynamic variants

--- src/framer-components/EmojisAnimation.jsx
+++ src/framer-components/EmojisAnimation.jsx
@@ -4,10 +4,6 @@ import { motion } from "framer-motion";
 const Variant1 = {
   show: {
     opacity: 1,
-    transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.3,
-    },
   },
   hide: {
     opacity: 0,
@@ -18,15 +14,20 @@ const Variant1 = {
 };
 
 const Variant2 = {
-  show: {
+  show: (i) => ({
     opacity: 1,
     x: 0,
-  },
+    transition: {
+      delay: i * 2,
+    },
+  }),
   hide: {
     opacity: 0,
     x: -100,
   },
 };
+
+const emojisArr = ["😆", "💯", "❤️", "😇", "👍"];
 
 function EmojisAnimation() {
   const [showEmojis, setShowEmojis] = useState(false);
@@ -42,21 +43,18 @@ function EmojisAnimation() {
           className="flex rounded-full p-4 items-center text-3xl justify-evenly w-64 h-12 bg-white"
         >
           {/* some emojis */}
-          <motion.span variants={Variant2} className="bg-transparent">
-            😆
-          </motion.span>
-          <motion.span variants={Variant2} className="bg-transparent">
-            ❤️
-          </motion.span>
-          <motion.span variants={Variant2} className="bg-transparent">
-            💯
-          </motion.span>
-          <motion.span variants={Variant2} className="bg-transparent">
-            👍
-          </motion.span>
-          <motion.span variants={Variant2} className="bg-transparent">
-            😇
-          </motion.span>
+          {emojisArr.map((emoji, index) => {
+            return (
+              <motion.span
+                key={index}
+                custom={index}
+                variants={Variant2}
+                className="bg-transparent"
+              >
+                {emoji}
+              </motion.span>
+            );
+          })}
         </motion.div>
         <button
           className="bg-sky-500 text-white cursor-pointer font-semibold py-2 px-8 rounded-md"
